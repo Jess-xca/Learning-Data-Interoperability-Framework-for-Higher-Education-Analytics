a0a66e59b751edffd3259e1890815f9aa251be3f
Jess-xca <jessicairakoze4@gmail.com>
2026-04-03 16:09:55 +0200

    feat: add component barrel exports for clean imports

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,15 @@
+// Layout
+export * from './layout';
+export { Sidebar, Header, MainContent } from './layout';
+
+// Forms
+export * from './forms';
+export { Button, TextInput, SelectInput } from './forms';
+
+// Common
+export * from './common';
+export { Card, Badge, Alert } from './common';
+
+// Dashboard
+export * from './dashboard';
+export { MetricCard } from './dashboard';
