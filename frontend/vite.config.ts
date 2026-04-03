1fc9fb8b182db9aed71c38c27077f163aac6cb7f
Jess-xca <jessicairakoze4@gmail.com>
2026-04-04 01:26:47 +0200

    build: enable code splitting to optimize bundle size

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 750,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules/react")) {
+            return "react-vendor";
+          }
+          if (
+            id.includes("node_modules/@reduxjs") ||
+            id.includes("node_modules/react-redux")
+          ) {
+            return "redux-vendor";
+          }
+        },
+      },
+    },
+  },
+});
